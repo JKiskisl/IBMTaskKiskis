2bcfe9ee7741857a803378b5f9d35a14100fa630
JKiskisl <smicscl@gmail.com>
2023-09-05 10:53:26 +0300

    responsive ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
-import Header from "./components/header/header";
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Header from "../src/components/header/header";
+import CryptoDashboard from "../src/components/cryptochart/cryptoDashboard";
 
-import CryptoDashboard from "./components/cryptochart/cryptoDashboard";
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Header />
-      <CryptoDashboard />
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <CryptoDashboard />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
